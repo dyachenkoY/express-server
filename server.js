8abae999e849869884ea1bf68bbb2ab2b2bdb510
“dyachenkoY” <diachenko.yevhenii93@gmail.com>
2022-12-20 20:30:55 +0200

    Add user creation route

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 const express = require("express");
+const yup = require("yup");
 
 const app = express();
 
 const PORT = 5000;
 
+const usersDB = [];
+
+const bodyParser = express.json();
+
+const USER_CREATION_SCHEMA = yup.object({
+  login: yup.string().required(),
+  password: yup.string().required(),
+});
+
 app.get("/users", (req, res) => {
-  res.send([{ id: 1 }, { id: 2 }]);
+  res.send(usersDB);
 });
 
 app.post(
   "/users",
-  (req, res, next) => {
-    console.log("first middleware");
-    next();
+  bodyParser,
+  async (req, res, next) => {
+    try {
+      const validateUser = await USER_CREATION_SCHEMA.validate(req.body);
+      next();
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
   },
-  (req, res, next) => {
-    console.log("second middleware");
-    next();
-  },
-  (req, res, next) => {
-    console.log("final func");
-    res.send("user created");
+  async (req, res) => {
+    const newUser = {
+      ...req.body,
+      id: Date.now(),
+    };
+
+    usersDB.push(newUser);
+    res.send(newUser);
   }
 );
 
