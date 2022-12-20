37daf490022344aafd01e901bfb82e35ce4c6a0a
“dyachenkoY” <diachenko.yevhenii93@gmail.com>
2022-12-20 19:47:42 +0200

    add middleware

--- server.js
+++ server.js
@@ -8,8 +8,31 @@ app.get("/users", (req, res) => {
   res.send([{ id: 1 }, { id: 2 }]);
 });
 
+app.post(
+  "/users",
+  (req, res, next) => {
+    console.log("first middleware");
+    next();
+  },
+  (req, res, next) => {
+    console.log("second middleware");
+    next();
+  },
+  (req, res, next) => {
+    console.log("final func");
+    res.send("user created");
+  }
+);
+
 app.get("/test*", (req, res) => {
   res.send(`request.path is ${req.path} and request.method is ${req.method}`);
 });
+
+/* 
+api.post
+api.put
+api.patch
+api.
+*/
 
 app.listen(PORT);
