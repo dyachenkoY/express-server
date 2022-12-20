eb0bda0d58b4d97269c78667fa7cf4b9f646d932
“dyachenkoY” <diachenko.yevhenii93@gmail.com>
2022-12-20 19:04:09 +0200

    add some examples

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/users", (req, res) => {
+  res.send([{ id: 1 }, { id: 2 }]);
+});
+
+app.get("/test*", (req, res) => {
+  res.send(`request.path is ${req.path} and request.method is ${req.method}`);
+});
+
+app.listen(PORT);
